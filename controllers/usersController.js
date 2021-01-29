0cf608a3d73f3cb75cd748d766592d63eeb843e9
Lionhatt <gavindxy@gmail.com>
2021-01-30 01:21:44 +1100

    add usercontroller

---
+++ controllers/usersController.js
@@ -0,0 +1,23 @@
+const db = require('../models');
+
+// Defining methods for the usersController
+module.exports = {
+    create: function (req, res) {
+        db.User
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findOne: function (req, res) {
+        db.User
+            .findOne({ username: req.params.id })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        db.User
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+}
