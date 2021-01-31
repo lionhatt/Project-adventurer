d4a1c6b662446868c20bdfca28ed0096300d9e44
Lionhatt <gavindxy@gmail.com>
2021-02-01 07:01:44 +1100

    add trails routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require('./users');
+const trailRoutes = require('./trails')
 
 //User routes
 router.use('/users', userRoutes);
+router.use('/trails', trailRoutes)
 
 module.exports = router
